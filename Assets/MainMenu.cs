107e4decd3da1f7aeaa2b8666f55d4ba0e6f2a46
RobotOrtem <wegasd10@gmail.com>
2020-06-01 19:43:06 +0300

    First commit

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MapMode");
+    }
+    public void OpenSettings()
+    {
+        SettingsPanel.SetActive(true);
+    }
+    public void CloseSettings()
+    {
+        SettingsPanel.SetActive(false);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
