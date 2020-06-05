11469fb0abaea9bacf2796b2a0289e5770eeff08
RobotOrtem <wegasd10@gmail.com>
2020-06-05 18:32:09 +0300

    Nevermind

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Player
+{
+    public static float MaxFood;
+    public static float MaxWater;
+    public static float MaxHP;
+    public static float MaxMP;
+    public static float MaxST;
+
+    public static float HP;
+    public static float MP;
+    public static float ST;
+    public static float Food;
+    public static float Water;
+
+    public static float SumBaseDamage;
+
+    public static float XPBonus;
+    public static float HPBonus;
+    public static float STBonus;
+    public static float MPBonus;
+
+    public static float RegSpeedHP;
+    public static float RegSpeedMP;
+    public static float RegSpeedST;
+
+    public static float CraftTalent;
+    public static float PhysicalFightTalent;
+    public static float ManaFightTalent;
+    public static float GodlessTalent;
+
+
+}
